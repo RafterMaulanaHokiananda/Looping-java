9c94fc9ab4efcc32b53a2fec442293d44bde06a3
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-08-02 11:14:02 +0700

    for , while , do while

---
+++ src/jo8/Contoh01.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jo8;
+
+/**
+ *
+ * @author Rafter PC
+ */
+public class Contoh01 {
+    public static void main(String[] args) {
+        //PERULANGAN FOR
+        int x;
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.println("SMK TELKOM MALANG");
+        }
+        //PERULANGAN WHILE
+        int i = 1;
+                while(i <= 5)
+                {
+                    System.out.println("SMK TELKOM MALANG");
+                    i++;
+                }
+                
+        //PERULANGAN DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK TELKOM MALANG");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
